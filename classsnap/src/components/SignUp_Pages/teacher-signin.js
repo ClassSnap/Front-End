ecee8ec1ddd99d10ebb7afaff39b446c982a58ae
karenjli <karenjy.li@gmail.com>
2019-11-01 20:36:45 -0500

    removed credentials hook

--- classsnap/src/components/SignUp_Pages/teacher-signin.js
+++ classsnap/src/components/SignUp_Pages/teacher-signin.js
@@ -6,8 +6,6 @@ import axios from "axios";
 import * as Yup from "yup";
 
 const Login = ({ errors, touched }) => {
-  const [credentials, setCredentials] = useState();
-
   return (
     <div className="sign-in-form">
       <Form>
