6253b48e0e7783eb5de74f67d405d8caef994949
karenjli <karenjy.li@gmail.com>
2020-01-05 10:39:16 -0600

    Redirect on landing page fixed

--- classsnap/src/components/SignUp_Pages/landing.js
+++ classsnap/src/components/SignUp_Pages/landing.js
@@ -30,34 +30,50 @@ const LandingPage = () => {
   //   }
   //   rightPage();
   // }, []);
-  return (
-    <div className="landing-page">
-      <div className="onboard-top">
-        <h2>Welcome to ClassSnap</h2>
-        <h3>Get Started with selecting your user type</h3>
-        <Buttoncontainer>
-          <Link to="/teachersignup">
-            <Button primary>Teacher Register</Button>
-          </Link>
-          <Link to="/parentsignup">
-            <Button secondary>Parent Register</Button>
-          </Link>
-        </Buttoncontainer>
-      </div>
-      <Divider />
-      <div className="onboard-bottom">
-        <h3>Already have an account? Login below.</h3>
-        <Buttoncontainer>
-          <Link to="/teacherlogin">
-            <Button primary>Teacher Login</Button>
-          </Link>
-          <Link to="/parentlogin">
-            <Button secondary>Parent Login</Button>
-          </Link>
-        </Buttoncontainer>
-      </div>
-    </div>
-  );
+  {
+    if (localStorage.getItem("token")) {
+      return <Redirect to="/teacher/dashboard" />;
+    } else if (
+      localStorage.getItem("parentToken") &&
+      localStorage.getItem("language") === "Spanish"
+    ) {
+      return <Redirect to="/parent/spn/dashboard" />;
+    } else if (
+      localStorage.getItem("parentToken") &&
+      localStorage.getItem("language") !== "Spanish"
+    ) {
+      return <Redirect to="/parent/dashboard" />;
+    } else {
+      return (
+        <div className="landing-page">
+          <div className="onboard-top">
+            <h2>Welcome to ClassSnap</h2>
+            <h3>Get Started with selecting your user type</h3>
+            <Buttoncontainer>
+              <Link to="/teachersignup">
+                <Button primary>Teacher Register</Button>
+              </Link>
+              <Link to="/parentsignup">
+                <Button secondary>Parent Register</Button>
+              </Link>
+            </Buttoncontainer>
+          </div>
+          <Divider />
+          <div className="onboard-bottom">
+            <h3>Already have an account? Login below.</h3>
+            <Buttoncontainer>
+              <Link to="/teacherlogin">
+                <Button primary>Teacher Login</Button>
+              </Link>
+              <Link to="/parentlogin">
+                <Button secondary>Parent Login</Button>
+              </Link>
+            </Buttoncontainer>
+          </div>
+        </div>
+      );
+    }
+  }
 };
 
 export default LandingPage;
